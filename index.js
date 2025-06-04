8788f30f511ed91f4fd131f9419d28912808ec7a
Ajay-K-coder <ak4794033@gmail.com>
2025-06-04 13:00:32 -0400

    add new button

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let body = document.querySelector("body");
 
-let button = document.createElement("<button>");
+let button = document.createElement("button");
+
+button.innerText = "click me"
 
 body.appendChild(button);
